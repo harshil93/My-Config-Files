8d070859e1a9da9dd9e46371150fe00921c5c0ba
Harshil Lodhi <harslo@microsoft.com>
2016-09-14 13:56:09 +0530

    powershell psreadline enhancements

--- gitaliases.ps1
+++ gitaliases.ps1
@@ -26,9 +26,15 @@ function Git-Log-Graph(){
 	git log --oneline --graph
 }
 
+function Git-PushCurrentBranch($arg){
+    $branchName = $(git rev-parse --abbrev-ref HEAD)
+    git push origin $branchName $arg
+}
+
 New-Alias gil Git-Log
 New-Alias gilg Git-Log-Graph
 New-Alias gis Git-Status
 New-Alias gia Git-Add
 New-Alias glci LastCommitID
 New-Alias gcbr Git-CurrentBranch
+New-Alias gpcbr Git-PushCurrentBranch
--- loader.ps1
+++ loader.ps1
@@ -1,2 +1,3 @@
 . $PSScriptRoot/gitaliases.ps1
 . $PSScriptRoot/myaliases.ps1
+. $PSScriptRoot/psreadline-config.ps1
