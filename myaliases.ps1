c0138bd1f997ffb171195911616309cc99f680c2
Harshil Lodhi <harslo@microsoft.com>
2016-09-15 09:57:09 +0530

    cd dash added

--- myaliases.ps1
+++ myaliases.ps1
@@ -1,3 +1,20 @@
 function youtube-dl-mp3 {
     youtube-dl --extract-audio --audio-format mp3 $args
 }
+
+
+function cddash {
+    if ($args[0] -eq '-') {
+        $pwd = $OLDPWD;
+    } else {
+        $pwd = $args[0];
+    }
+    $tmp = pwd;
+
+    if ($pwd) {
+        Set-Location $pwd;
+    }
+    Set-Variable -Name OLDPWD -Value $tmp -Scope global;
+}
+
+Set-Alias -Name cd -value cddash -Option AllScope
