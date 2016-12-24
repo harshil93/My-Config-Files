2cdcdc7c64c5174adb9fd7520cf6d5958b038481
Harshil Lodhi <harslo@microsoft.com>
2016-12-24 12:01:04 +0530

    Powershell enhancements

--- loader.ps1
+++ loader.ps1
@@ -1,3 +1,5 @@
+. $PSScriptRoot/psreadline-config.ps1
+. $PSScriptRoot/powershell-enhancements.ps1
+. $PSScriptRoot/Get-ChildItem-Color.ps1
 . $PSScriptRoot/gitaliases.ps1
 . $PSScriptRoot/myaliases.ps1
-. $PSScriptRoot/psreadline-config.ps1
--- myaliases.ps1
+++ myaliases.ps1
@@ -1,5 +1,5 @@
 function youtube-dl-mp3 {
-    youtube-dl --extract-audio --audio-format mp3 $args
+    youtube-dl --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s" $args
 }
 
 
